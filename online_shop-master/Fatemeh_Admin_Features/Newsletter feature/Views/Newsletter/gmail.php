f849160d00fe63451c34a7b28378833022bad387
fatemehabdizadeh <n01137646@humbermail.ca>
2016-04-21 13:34:37 -0400

    Newsletter

---
+++ online_shop-master/Fatemeh_Admin_Features/Newsletter feature/Views/Newsletter/gmail.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * This example shows settings to use when sending via Google's Gmail servers.
+ */
+/*if(isset($_GET['email']))
+$email=$_GET['email'];
+if(isset($_GET['message']))
+$message=$_GET['message'];*/
+//SMTP needs accurate times, and the PHP time zone MUST be set
+//This should be done in your php.ini, but this is how to do it if you don't have access to that
+//echo sendenailgmail('fatemeh.abdizadeh@yahoo.com','This is my message');
+
+require 'PHPMailerAutoload.php';
+function sendenailgmail($email,$message){
+
+
+date_default_timezone_set('Etc/UTC');
+
+
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 0;
+
+//Ask for HTML-friendly debug output
+$mail->Debugoutput = 'html';
+
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "abdizadeh.fatemeh@gmail.com";
+
+//Password to use for SMTP authentication
+$mail->Password = "sohrab1930";
+
+//Set who the message is to be sent from
+$mail->setFrom('from@example.com', 'First Last');
+
+//Set an alternative reply-to address
+$mail->addReplyTo('replyto@example.com', 'First Last');
+
+//Set who the message is to be sent to
+$mail->addAddress($email);
+
+//Set the subject line
+$mail->Subject = 'PHPMailer GMail SMTP test';
+
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->msgHTML($message);
+
+//Replace the plain text body with one created manually
+$mail->AltBody = 'This is a plain-text message body';
+
+//Attach an image file
+$mail->addAttachment('images/phpmailer_mini.png');
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: ";
+} else {
+    echo "Message sent!";
+}
+
+}
