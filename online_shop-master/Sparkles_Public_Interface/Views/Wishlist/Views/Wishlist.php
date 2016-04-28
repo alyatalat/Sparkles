a2bf222ec848f85457e2ab0cd3100a3fef3e597e
LoveleenAnand <loveleen.anand@outlook.com>
2016-04-27 22:40:13 -0400

    change made on wishlist was displaying unneccessary error

--- online_shop-master/Sparkles_Public_Interface/Views/Wishlist/Views/Wishlist.php
+++ online_shop-master/Sparkles_Public_Interface/Views/Wishlist/Views/Wishlist.php
@@ -11,20 +11,7 @@ if(isset($_GET['id']))
 }
 $product[] = wishlistsDB::getProduct($id);
 
-session_start();
-require_once("../../Customerlogin/Models/customer.php");
-$auth_user = new customer();
-if(isset($_SESSION['user_session'])) {
 
-    $user_id = $_SESSION['user_session'];
-    $stmt = $auth_user->runQuery("SELECT * FROM users WHERE user_id=:user_id");
-    $stmt->execute(array(":user_id" => $user_id));
-    $userRow = $stmt->fetch(PDO::FETCH_ASSOC);
-    var_dump("user exists". $user_id );
-}
-else{
-    var_dump("error");
-}
 
 ?>
 <div class="container-fluid">
